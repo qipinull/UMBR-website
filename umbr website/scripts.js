f25a9ecc1c3f90f8ff55f3292bf8dbec2709528e
qipi <ebroberts9@icloud.com>
2024-09-15 12:26:43 +0100

    Delete umbr website directory

--- umbr website/scripts.js
+++
@@ -1,8 +0,0 @@
-function reviews() {
-    var x = document.getElementById("reviews");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
